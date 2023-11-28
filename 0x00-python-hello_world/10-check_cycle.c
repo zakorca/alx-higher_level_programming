fd3904682070a011c05ea4751cb15bece361f1a9
zakorca <zakariatouras47.com>
2023-11-27 18:49:35 -0800

    task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * check_cycle - check if the list is cycly or not
+ * @list: head.
+ * Return: 0 if there is no cycle, 1 if there is a cycle.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}
