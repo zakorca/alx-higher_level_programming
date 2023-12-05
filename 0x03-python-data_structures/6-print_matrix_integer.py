505a2657e2657d0531d36ef7fc7d0911ebc65b6b
zakorca <zakariatouras47.com>
2023-12-04 18:25:40 -0800

    task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print()
+    for row in matrix:
+        for elem in row:
+            if elem != row[-1]:
+                print("{:d}".format(elem), end=" ")
+            else:
+                print("{:d}".format(elem))
