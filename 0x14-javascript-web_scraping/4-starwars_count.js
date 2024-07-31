7dd89a0442ac4e2c0bdbc1fd0e1ed7246b47f2f3
ZAKARIA TOURAS <zakariatouras47@gmail.com>
2024-07-30 18:49:21 -0700

    0task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+request.get(process.argv[2], function (err, resp, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    let count = 0;
+    const content = JSON.parse(body);
+    for (let i = 0; content.results[i] !== undefined; i++) {
+      if (content.results[i].characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+        count++;
+      }
+    }
+    console.log(count);
+  }
+});
